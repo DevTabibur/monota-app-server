e07643f8ee3c5c15a749260d44010b3f2686ca6f
DevTabibur <tobiburrohman2@gmail.com>
2022-06-04 21:02:42 +0600

    again start

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json()); // Or app.use(express.json())
 
 const PORT = process.env.PORT || 5000;
-
+// try updating
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hc4xz.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
